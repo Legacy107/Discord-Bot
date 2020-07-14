53ef913df6d2e620c481bd65280fb3f3ce2800ca
Legacy107 <49741920+Legacy107@users.noreply.github.com>
2020-07-14 20:52:38 +0700

    Added listpic command

--- Cogs/Picture.py
+++ Cogs/Picture.py
@@ -17,102 +17,112 @@ class Picture(commands.Cog):
 		self.font_dir = os.path.join('..', 'font', 'VNF-Comic Sans.ttf')	#  ..\font\VNF-Comic Sans.ttf
 
 	def resize(self, image_name):
-	    image = Image.open('%s%s' % (self.images_dir, image_name))
-	    width = int(float(image.size[0]) * (global_var.max_height / float(image.size[1])))
-	    image = image.resize((width, global_var.max_height), Image.ANTIALIAS)
-	    image.save('%s%s' % (self.images_dir, image_name))
-	    image.close()
+		image = Image.open('%s%s' % (self.images_dir, image_name))
+		width = int(float(image.size[0]) * (global_var.max_height / float(image.size[1])))
+		image = image.resize((width, global_var.max_height), Image.ANTIALIAS)
+		image.save('%s%s' % (self.images_dir, image_name))
+		image.close()
 
 	@commands.command(name='savepic', help='Save attached image. Syntax: >savepic <name> + <image>')
 	async def _savepic(self, ctx, name: str):
-	    name = name.lower()
-	    attachment = ctx.message.attachments
-	    if len(attachment) == 0:
-	        await ctx.send('Pls attach an image')
-	        return
-	    if len(attachment) > 1:
-	        await ctx.send('Attach 1 image only')
-	        return
-	    file = shelve.open(self.data_dir)
-	    if name in file['image'].keys():
-	        await ctx.send('Invalid name')
-	        return
-	    extension = attachment[0].url.split('.')[-1]
-	    all_image = file['image']
-	    all_image[name] = extension
-	    file['image'] = all_image
-	    file.close()
-	    image_name = '.'.join((name, extension))
-	    await attachment[0].save('%s%s' % (self.images_dir, image_name), seek_begin=True, use_cached=False)
-	    if attachment[0].height > global_var.max_height:
-	        resize(image_name)
-	    await ctx.send('Saved image as %s' % image_name)
+		name = name.lower()
+		attachment = ctx.message.attachments
+		if len(attachment) == 0:
+			await ctx.send('Pls attach an image')
+			return
+		if len(attachment) > 1:
+			await ctx.send('Attach 1 image only')
+			return
+		file = shelve.open(self.data_dir)
+		if name in file['image'].keys():
+			await ctx.send('Invalid name')
+			return
+		extension = attachment[0].url.split('.')[-1]
+		all_image = file['image']
+		all_image[name] = extension
+		file['image'] = all_image
+		file.close()
+		image_name = '.'.join((name, extension))
+		await attachment[0].save('%s%s' % (self.images_dir, image_name), seek_begin=True, use_cached=False)
+		if attachment[0].height > global_var.max_height:
+			resize(image_name)
+		await ctx.send('Saved image as %s' % image_name)
 
 
 	@commands.command(name='delpic', help='Delete a pic saved with >savepic. Syntax: >delpic <name>')
 	async def _delpic(self, ctx, name: str):
-	    name = name.lower()
-	    file = shelve.open(self.data_dir)
-	    all_images = file['image']
-	    if name not in all_images.keys():
-	        await ctx.send('Pic not found')
-	        return
-	    extension = all_images[name]
-	    del all_images[name]
-	    file['image'] = all_images
-	    file.close()
-	    image_name = '.'.join((name, extension))
-	    if os.path.exists('%s%s' % (self.images_dir, image_name)):
-	        os.remove('%s%s' % (self.images_dir, image_name))
-	    await ctx.send('Removed %s' % image_name)
+		name = name.lower()
+		file = shelve.open(self.data_dir)
+		all_images = file['image']
+		if name not in all_images.keys():
+			await ctx.send('Pic not found')
+			return
+		extension = all_images[name]
+		del all_images[name]
+		file['image'] = all_images
+		file.close()
+		image_name = '.'.join((name, extension))
+		if os.path.exists('%s%s' % (self.images_dir, image_name)):
+			os.remove('%s%s' % (self.images_dir, image_name))
+		await ctx.send('Removed %s' % image_name)
 
+	
+	@commands.command(name='listpic', help='List all pics. Syntax: >listpic')
+	async def _listpic(self, ctx):
+		file = shelve.open(self.data_dir)
+		text = '`| '
+		for name in file['image'].keys():
+			text += name + ' | '
+		text += '`'
+		file.close()
+		await ctx.send(text)
 
 	@commands.command(name='pic', help='Send a pic saved with >savepic. Syntax: >pic <name>')
 	async def _pic(self, ctx, name: str):
-	    name = name.lower()
-	    file = shelve.open(self.data_dir)
-	    if name not in file['image'].keys():
-	        await ctx.send('Pic not found')
-	        return
-	    extension = file['image'][name]
-	    file.close()
-	    await ctx.send(content='**%s said**' % ctx.message.author.name,
-	                   file=discord.File('%s%s.%s' % (self.images_dir, name, extension)))
+		name = name.lower()
+		file = shelve.open(self.data_dir)
+		if name not in file['image'].keys():
+			await ctx.send('Pic not found')
+			return
+		extension = file['image'][name]
+		file.close()
+		await ctx.send(content='**%s said**' % ctx.message.author.name,
+					   file=discord.File('%s%s.%s' % (self.images_dir, name, extension)))
 
 
 	@commands.command(name='hhh', help=u'Huấn Rô Sì')
 	async def _hhh(self, ctx, *, arg):
-	    image_name = 'hhh.png'
-	    font = ImageFont.truetype(self.font_dir, 18)
-	    header = u'Này {users}! Anh Huấn tặng chú 1 câu'
-	    users = ctx.message.mentions
-	    names = [user.name for user in users]
-	    text = ' '.join(arg.split()[len(users):]) + u' em nhé!'  # remove mentions
-	    max_letters = 23
-	    color = 'black'
+		image_name = 'hhh.png'
+		font = ImageFont.truetype(self.font_dir, 18)
+		header = u'Này {users}! Anh Huấn tặng chú 1 câu'
+		users = ctx.message.mentions
+		names = [user.name for user in users]
+		text = ' '.join(arg.split()[len(users):]) + u' em nhé!'  # remove mentions
+		max_letters = 23
+		color = 'black'
 
-	    image = Image.open('%s%s' % (self.images_dir, image_name))
-	    image_width, image_height = image.size
-	    draw = ImageDraw.Draw(image)
+		image = Image.open('%s%s' % (self.images_dir, image_name))
+		image_width, image_height = image.size
+		draw = ImageDraw.Draw(image)
 
-	    header = header.format(users=', '.join(names))
-	    para = textwrap.wrap(header, width=max_letters)
-	    current_height = 0
-	    for line in para:
-	        width, height = draw.textsize(line, font=font)
-	        draw.text(((image_width - width) / 2, current_height), line, font=font, fill=color)
-	        current_height += height
+		header = header.format(users=', '.join(names))
+		para = textwrap.wrap(header, width=max_letters)
+		current_height = 0
+		for line in para:
+			width, height = draw.textsize(line, font=font)
+			draw.text(((image_width - width) / 2, current_height), line, font=font, fill=color)
+			current_height += height
 
-	    para = textwrap.wrap(text, width=max_letters)[::-1]  # print in reverse order
-	    print(para)
-	    current_height = image_height - 25
-	    for line in para:
-	        width, height = draw.textsize(line, font=font)
-	        draw.text(((image_width - width) / 2, current_height), line, font=font, fill=color)
-	        current_height -= height
+		para = textwrap.wrap(text, width=max_letters)[::-1]  # print in reverse order
+		print(para)
+		current_height = image_height - 25
+		for line in para:
+			width, height = draw.textsize(line, font=font)
+			draw.text(((image_width - width) / 2, current_height), line, font=font, fill=color)
+			current_height -= height
 
-	    image.save('%stmp%s' % (self.images_dir, image_name))
-	    await ctx.send(file=discord.File('%stmp%s' % (self.images_dir, image_name)))	
+		image.save('%stmp%s' % (self.images_dir, image_name))
+		await ctx.send(file=discord.File('%stmp%s' % (self.images_dir, image_name)))	
 
 def setup(bot):
 	bot.add_cog(Picture(bot))
